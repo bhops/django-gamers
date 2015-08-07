2bad0365236bd63f4cc9ee45507fc38e29e8ae44
bhops <hops.brandon@gmail.com>
2015-08-07 01:57:48 -0400

    Added metacritic game description lookup

--- games/models.py
+++ games/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import pre_save
-from  abc
+from django.dispatch import receiver
+import requests
 
 class Platform(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +27,14 @@ class Game(models.Model):
 
     class Meta:
         ordering = ('title',)
+
+@receiver(pre_save, sender=Game)
+def get_game_details(sender, **kwargs):
+    instance = kwargs.get('instance', None)
+    if instance is not None:
+        url = 'https://metacritic-2.p.mashape.com/find/game?platform='+instance.platform.name.lower()+'&title='+instance.title
+        headers = {'X-Mashape-Key': 'VR9AsO5oummsh0k12JNdlbnzAzj0p1s152ejsnyjkKlJ2U5hI3', 'Accept': 'application/json'}
+        r = requests.get(url, headers=headers)
+        json = r.json()
+        result = json.get('result', None)
+        instance.description = result.get('summary', 'No Description Available') if result else 'No Description Available'
