d6b6ac3952b0461ecc06798922e0703fda24cc1d
bhops <hops.brandon@gmail.com>
2015-09-05 23:14:48 -0700

    squash games migrations

--- games/migrations/0001_squashed_0009_auto_20150906_0609.py
+++ games/migrations/0001_squashed_0009_auto_20150906_0609.py
@@ -7,6 +7,8 @@ from django.conf import settings
 
 class Migration(migrations.Migration):
 
+    replaces = [('games', '0001_initial'), ('games', '0002_game_slug'), ('games', '0003_auto_20150830_0559'), ('games', '0004_remove_game_slug'), ('games', '0005_game_slug'), ('games', '0006_auto_20150830_0652'), ('games', '0007_platform_slug'), ('games', '0008_auto_20150905_1845'), ('games', '0009_auto_20150906_0609')]
+
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
@@ -15,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Game',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.TextField()),
                 ('released', models.DateTimeField()),
@@ -26,21 +28,11 @@ class Migration(migrations.Migration):
             },
         ),
         migrations.CreateModel(
-            name='OwnedGame',
-            fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
-                ('game', models.ForeignKey(to='games.Game')),
-                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-            ],
-        ),
-        migrations.CreateModel(
             name='Platform',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
                 ('name', models.CharField(max_length=100)),
-                ('company', models.CharField(max_length=100)),
-                ('description', models.TextField()),
-                ('released', models.DateField(auto_now_add=True)),
+                ('slug', models.SlugField(default='pc', unique=True)),
             ],
             options={
                 'ordering': ('name',),
@@ -50,5 +42,15 @@ class Migration(migrations.Migration):
             model_name='game',
             name='platform',
             field=models.ForeignKey(related_name='games', to='games.Platform'),
+        ),
+        migrations.AddField(
+            model_name='game',
+            name='slug',
+            field=models.SlugField(unique=True),
+        ),
+        migrations.AlterField(
+            model_name='game',
+            name='released',
+            field=models.DateField(auto_now_add=True),
         ),
     ]
