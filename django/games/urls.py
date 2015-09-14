b97015e534da538c7de015f82bb1715be9ae167b
bhops <hops.brandon@gmail.com>
2015-09-13 18:04:14 -0700

    update to viewsets

--- django/games/urls.py
+++ django/games/urls.py
@@ -1,12 +1,21 @@
-from django.conf.urls import url
-from rest_framework.urlpatterns import format_suffix_patterns
+#from django.conf.urls import url
+from rest_framework import routers
+#from rest_framework.urlpatterns import format_suffix_patterns
 from games import views
 
-urlpatterns = [
-    url(r'^games$', views.GameList.as_view(), name='game-list'),
-    url(r'^games/(?P<slug>[-\w]+)$', views.GameDetail.as_view(), name='game-detail'),
-    url(r'^platforms$', views.PlatformList.as_view(), name='platform-list'),
-    url(r'^platforms/(?P<slug>[-\w]+)$', views.PlatformDetail.as_view(), name='platform-detail'),
-]
+router = routers.DefaultRouter(trailing_slash=False)
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+router.register(r'platforms', views.PlatformViewSet)
+router.register(r'games', views.GameViewSet)
+
+urlpatterns = router.urls
+
+
+#urlpatterns = [
+#    url(r'^games$', views.GameList.as_view(), name='game-list'),
+#    url(r'^games/(?P<slug>[-\w]+)$', views.GameDetail.as_view(), name='game-detail'),
+#   url(r'^platforms$', views.PlatformList.as_view(), name='platform-list'),
+#    url(r'^platforms/(?P<slug>[-\w]+)$', views.PlatformDetail.as_view(), name='platform-detail'),
+#]
+
+#urlpatterns = format_suffix_patterns(urlpatterns)
--- django/games/views.py
+++ django/games/views.py
@@ -1,38 +1,24 @@
-from rest_framework import generics, mixins
+from rest_framework import mixins, viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import list_route
 from games.models import Game, Platform
 from games.serializers import GameSerializer, PlatformSerializer
 
-class GameList(mixins.ListModelMixin,
-               generics.GenericAPIView):
+class GameViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
     serializer_class = GameSerializer
     queryset = Game.objects.all()
 
-    def get(self, request, *args, **kwargs):
-       return self.list(request, *args, **kwargs)
-
-class GameDetail(mixins.RetrieveModelMixin,
-                 generics.GenericAPIView):
-    lookup_field = 'slug'
-    serializer_class = GameSerializer
-    queryset = Game.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-
-class PlatformList(mixins.ListModelMixin,
-                   generics.GenericAPIView):
+class PlatformViewSet(mixins.ListModelMixin,
+                      mixins.RetrieveModelMixin,
+                      viewsets.GenericViewSet):
     serializer_class = PlatformSerializer
     queryset = Platform.objects.all()
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+    @list_route()
+    def games(self, request, *args, **kwargs):
+        platform = self.queryset[0]
+        games = platform.games
 
-class PlatformDetail(mixins.RetrieveModelMixin,
-                     generics.GenericAPIView):
-    lookup_field = 'slug'
-    serializer_class = PlatformSerializer
-    queryset = Platform.objects.all()
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        return Response(GameSerializer(games, many=True).data)
